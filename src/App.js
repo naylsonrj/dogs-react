449574ff54cf9e388e1ecc15556c4d536d80ffcf
naylson <naylsonrj@gmail.com>
2022-11-30 20:59:40 -0300

    testando endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './App.css';
+import { Api } from './api/Api';
 
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <Api />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/Api.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserPost from './endpoints/UserPost';
+
+export const Api = () => {
+  return (
+    <div>
+      <h2>UserPost</h2>
+      <UserPost />
+    </div>
+  );
+};
