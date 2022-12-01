70db497fb0a85108426298cd9066649f17ab4760
naylson <naylsonrj@gmail.com>
2022-11-30 21:20:51 -0300

    endpoint photo

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import UserPost from './endpoints/UserPost';
+import TokenPost from './endpoints/TokenPost';
+import PhotoPost from './endpoints/PhotoPost';
 
 export const Api = () => {
   return (
     <div>
       <h2>UserPost</h2>
       <UserPost />
+      <br />
+      <h2>TokenPost</h2>
+      <TokenPost />
+      <h2>Photo Post</h2>
+      <PhotoPost />
     </div>
   );
 };
